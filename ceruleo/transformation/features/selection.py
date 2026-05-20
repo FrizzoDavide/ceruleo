3a5afc363e9e9ce38985a71ba542ef0e25b2d6b9
FrizzoDavide <davide.frizzo.1@studenti.unipd.it>
2026-05-20 08:17:00 +0200

    removed checkpoints from ByNameFeatureSelector

--- ceruleo/transformation/features/selection.py
+++ ceruleo/transformation/features/selection.py
@@ -29,15 +29,12 @@ class ByNameFeatureSelector(TransformerStep):
         else:
             features = list(set(df.columns))
 
-        ipdb.set_trace()
-
         if len(self.features_computed_) == 0:
             self.features_computed_ = features
         else:
             self.features_computed_ = [
                 f for f in self.features_computed_ if f in features
             ]
-        ipdb.set_trace()
         return self
 
     def fit(self, df:pd.DataFrame, y=None):
